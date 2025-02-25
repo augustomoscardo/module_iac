ac8950bd1ce2dc43e086cf2174b651fbdf02b099
Augusto Moscardo <augustomsaluz@gmail.com>
2025-02-24 23:30:51 -0300

    first resource - aws s3

--- index.ts
+++ index.ts
@@ -1,9 +1,14 @@
-import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
-import * as awsx from "@pulumi/awsx";
 
 // Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.BucketV2("my-bucket");
+const bucket = new aws.s3.BucketV2("primeiro-teste-bucket-pos-rocketseat", {
+  bucket: "primeiro-teste-bucket-pos-rocketseat",
+  tags: {
+    IAC: 'true'
+  }
+});
 
 // Export the name of the bucket
 export const bucketName = bucket.id;
+export const bucketInfo = bucket.bucket;
+export const bucketArn = bucket.arn;
