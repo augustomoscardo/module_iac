978f941d04fe0a99b3478dc22fa16872423e4208
Augusto Moscardo <augustomsaluz@gmail.com>
2025-02-26 22:08:54 -0300

    new: create new S3 bucket

--- index.ts
+++ index.ts
@@ -1,25 +1,36 @@
 import * as aws from "@pulumi/aws";
 
 // Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.BucketV2("primeiro-bucket", {
-  bucket: "primeiro-bucket-pos-tech-developer-360",
+const firstBucket = new aws.s3.BucketV2("primeiro-bucket", {
+  bucket: 'primeiro-bucket-pos-full-stack-360',
   tags: {
-    IAC: 'true'
-  }
+      IAC: "true",
+  },
+});
+
+const secondBucket = new aws.s3.BucketV2("segundo-bucket", {
+  bucket: 'segundo-bucket-pos-full-stack-360',
+  tags: {
+      IAC: "true",
+  },
 });
 
 const ecr = new aws.ecr.Repository("primeiro-ecr", {
-  name: "primeiro-ecr",
-  imageTagMutability: "IMMUTABLE",
+  name: 'primeiro-ecr',
+  imageTagMutability: 'IMMUTABLE',
   tags: {
-    IAC: 'true'
+      IAC: "true",
   }
-})
+});
 
 // Export the name of the bucket
-export const bucketName = bucket.id;
-export const bucketInfo = bucket.bucket;
-export const bucketArn = bucket.arn;
+export const firstBucketName = firstBucket.id;
+export const firstBucketRegion = firstBucket.region;
+export const firstBucketArn = firstBucket.arn;
+
+export const secondBucketName = secondBucket.id;
+export const secondBucketRegion = secondBucket.region;
+export const secondBucketArn = secondBucket.arn;
 
 export const ecrName = ecr.name;
 export const ecrRepositoryUrl = ecr.repositoryUrl;
