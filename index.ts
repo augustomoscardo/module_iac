4bedd253a735223437eac2b5406b3de563be76ac
Augusto Moscardo <augustomsaluz@gmail.com>
2025-02-27 22:30:16 -0300

    destroy resource

--- index.ts
+++ index.ts
@@ -8,12 +8,12 @@ const firstBucket = new aws.s3.BucketV2("primeiro-bucket", {
   },
 });
 
-const secondBucket = new aws.s3.BucketV2("segundo-bucket", {
-  bucket: 'segundo-bucket-tech-developer-stack-360',
-  tags: {
-    IAC: "true",
-  },
-});
+// const secondBucket = new aws.s3.BucketV2("segundo-bucket", {
+//   bucket: 'segundo-bucket-tech-developer-stack-360',
+//   tags: {
+//     IAC: "true",
+//   },
+// });
 
 const ecr = new aws.ecr.Repository("primeiro-ecr", {
   name: 'primeiro-ecr',
@@ -28,9 +28,9 @@ export const firstBucketName = firstBucket.id;
 export const firstBucketRegion = firstBucket.region;
 export const firstBucketArn = firstBucket.arn;
 
-export const secondBucketName = secondBucket.id;
-export const secondBucketRegion = secondBucket.region;
-export const secondBucketArn = secondBucket.arn;
+// export const secondBucketName = secondBucket.id;
+// export const secondBucketRegion = secondBucket.region;
+// export const secondBucketArn = secondBucket.arn;
 
 export const ecrName = ecr.name;
 export const ecrRepositoryUrl = ecr.repositoryUrl;
